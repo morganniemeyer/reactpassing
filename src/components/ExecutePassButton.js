d20ca45882117b6548521fc4acfef8074243ca24
Morgan Niemeyer <malineniemeyer@gmail.com>
2023-01-25 17:28:54 -0600

    slightly further code golf on pass button

--- src/components/ExecutePassButton.js
+++ src/components/ExecutePassButton.js
@@ -3,11 +3,10 @@ import Card from './Card';
 import { useGameContext } from '../context/GameContext.js';
 
 export default function ExecutePassButton({ passCard }) {
-  const { selectedCard, setSelectedCard, setFrom, from, to } = useGameContext();
+  const { selectedCard, setSelectedCard, from, to } = useGameContext();
   return (
     <div className="execute-button" onClick={() => passCard(selectedCard)}>
-      Pass <Card card={selectedCard} setSelectedCard={setSelectedCard} setFrom={setFrom} /> from{' '}
-      {from} to {to}
+      Pass <Card card={selectedCard} setSelectedCard={setSelectedCard} /> from {from} to {to}
     </div>
   );
 }
